37c490d25a483b8da1a6215545749b6d42a2d80e
andreitoledo <andreiltoledo@hotmail.com>
2023-06-25 15:06:02 -0300

    Implementando o Controller de Produtos

---
+++ src/Andreitoledo.GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Andreitoledo.GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Andreitoledo.GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        // Por ser microsserviços, a idéia não é referenciar todo o codigo da API
+        // e somente os métodos que utilizaram.
+        // Para esse método, busca na API o método FindAll em ProductController
+        public async Task<IActionResult> ProductIndex()
+        {
+            var products = _productService.FindAllProducts;
+            return View(products);
+        }
+    }
+}
