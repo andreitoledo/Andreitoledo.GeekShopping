50b920e5d4f3b2342e151b3ecfdea603670cfb74
andreitoledo <andreiltoledo@hotmail.com>
2023-06-25 00:42:16 -0300

    Implementando as Operações POST, PUT e DELETE

--- src/Andreitoledo.GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ src/Andreitoledo.GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -32,6 +32,29 @@ namespace Andreitoledo.GeekShopping.ProductAPI.Controllers
             return Ok(product);
         }
 
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            var product = await _repository.Create(vo);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            var product = await _repository.Update(vo);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if (!status) return BadRequest();
+            return Ok(status);
+        }
 
     }
 }
