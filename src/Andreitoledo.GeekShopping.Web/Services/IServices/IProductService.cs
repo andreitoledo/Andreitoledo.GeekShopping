57aa357c27ce41bf9df2574dabd4680cbcdc91b1
andreitoledo <andreiltoledo@hotmail.com>
2023-06-25 01:56:16 -0300

    Definindo Operações da Interface IProductService

---
+++ src/Andreitoledo.GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Andreitoledo.GeekShopping.Web.Models;
+
+namespace Andreitoledo.GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
