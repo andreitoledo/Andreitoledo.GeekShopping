0704aaca188d9a1710fb2c03d8189891300f388f
andreitoledo <andreiltoledo@hotmail.com>
2023-06-26 01:40:31 -0300

    Adicionando Configurações de Banco de Dados e Segurança

--- src/Andreitoledo.GeekShopping.IdentityServer/Program.cs
+++ src/Andreitoledo.GeekShopping.IdentityServer/Program.cs
@@ -1,3 +1,11 @@
+using Andreitoledo.GeekShopping.IdentityServer.Configuration;
+using Andreitoledo.GeekShopping.IdentityServer.Model;
+using Andreitoledo.GeekShopping.IdentityServer.Model.Context;
+using Duende.IdentityServer.AspNetIdentity;
+using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace Andreitoledo.GeekShopping.IdentityServer
 {
     public class Program
@@ -5,6 +13,31 @@ namespace Andreitoledo.GeekShopping.IdentityServer
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Conexão com o banco MySQL - andrei
+            var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+            builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+                connection,
+                new MySqlServerVersion(new Version(8, 0, 0))));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<MySQLContext>()
+                .AddDefaultTokenProviders();
+
+            var builderServices = builder.Services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.EmitStaticAudienceClaim = true;
+            })
+                .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+                .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+                .AddInMemoryClients(IdentityConfiguration.Clients)
+                .AddAspNetIdentity<ApplicationUser>();
+
+            builderServices.AddDeveloperSigningCredential();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -19,7 +52,7 @@ namespace Andreitoledo.GeekShopping.IdentityServer
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseIdentityServer();
             app.UseAuthorization();
 
             app.MapControllerRoute(
