567f055cadc60ddf5ffddc7941a3f908edc5535d
andreitoledo <andreiltoledo@hotmail.com>
2023-06-26 00:42:00 -0300

    Adição do DbContext e IdentityServer

---
+++ src/Andreitoledo.GeekShopping.IdentityServer/Model/Context/MySQLContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Andreitoledo.GeekShopping.IdentityServer.Model.Context
+{
+    public class MySQLContext : IdentityDbContext<ApplicationUser>
+    {        
+        public MySQLContext(DbContextOptions<MySQLContext> options)
+            : base(options) { }
+
+    }
+}
