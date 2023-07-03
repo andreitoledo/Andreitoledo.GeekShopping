75d8ee367ecbf651ca26d7f253ffad8355ce09cc
andreitoledo <andreiltoledo@hotmail.com>
2023-07-03 19:14:43 -0300

    Ajustando a Classe AccountController para Suportar o Login

--- src/Andreitoledo.GeekShopping.Web/Controllers/HomeController.cs
+++ src/Andreitoledo.GeekShopping.Web/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ namespace Andreitoledo.GeekShopping.Web.Controllers
         [Authorize]
         public async Task<IActionResult> Login()
         {
-            //var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
             return RedirectToAction(nameof(Index));
         }
 
