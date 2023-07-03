a4a0f092388d9c07f580ef76ff77e71682f609a4
andreitoledo <andreiltoledo@hotmail.com>
2023-07-03 16:56:16 -0300

    Adicionando as Configurações de Segurança ao Startup do GeekShopping.Web

--- src/Andreitoledo.GeekShopping.Web/Program.cs
+++ src/Andreitoledo.GeekShopping.Web/Program.cs
@@ -1,5 +1,6 @@
 using Andreitoledo.GeekShopping.Web.Services;
 using Andreitoledo.GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Andreitoledo.GeekShopping.Web
 {
@@ -17,6 +18,27 @@ namespace Andreitoledo.GeekShopping.Web
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            // Configuração para Autenticação
+            builder.Services.AddAuthentication(options => {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    options.ClientId = "geek_shopping";
+                    options.ClientSecret = "my_super_secret";
+                    options.ResponseType = "code";
+                    options.ClaimActions.MapJsonKey("role", "role", "role");
+                    options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+                    options.TokenValidationParameters.NameClaimType = "name";
+                    options.TokenValidationParameters.RoleClaimType = "role";
+                    options.Scope.Add("geek_shopping");
+                    options.SaveTokens = true;
+                });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -28,6 +50,8 @@ namespace Andreitoledo.GeekShopping.Web
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
