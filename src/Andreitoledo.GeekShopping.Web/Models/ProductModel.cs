54110efc038008f63a0d2ad1f5bc101c069be2a2
andreitoledo <andreiltoledo@hotmail.com>
2023-07-07 14:36:42 -0300

    Otimizando a Exibição da Listagem de Produtos

--- src/Andreitoledo.GeekShopping.Web/Models/ProductModel.cs
+++ src/Andreitoledo.GeekShopping.Web/Models/ProductModel.cs
@@ -5,8 +5,20 @@
         public long Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public string? Description { get; set; }
-        public string? CategoryName { get; set; }
-        public string? ImageURL { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageURL { get; set; }
+
+        public string SubstringName()
+        {
+            if (Name.Length < 24) return Name;
+            return $"{ Name.Substring(0, 21) } ...";
+        }
+
+        public string SubstringDescription()
+        {
+            if (Description.Length < 355) return Name;
+            return $"{Description.Substring(0, 352)} ...";
+        }
     }
 }
