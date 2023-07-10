66a1eb5116aef60f90912acbfa9f6d10b82ef512
andreitoledo <andreiltoledo@hotmail.com>
2023-07-10 18:46:09 -0300

    Ajuste no Controller II

--- src/Andreitoledo.GeekShopping.CartAPI/Controllers/CartController.cs
+++ src/Andreitoledo.GeekShopping.CartAPI/Controllers/CartController.cs
@@ -17,14 +17,14 @@ namespace Andreitoledo.GeekShopping.CartAPI.Controllers
         }
 
         [HttpGet("find-cart/{id}")]
-        public async Task<ActionResult<CartVO>> FindById(string userId)
+        public async Task<ActionResult<CartVO>> FindById(string id)
         {
-            var cart = await _repository.FindCartByUserId(userId);
+            var cart = await _repository.FindCartByUserId(id);
             if (cart == null) return NotFound();
             return Ok(cart);
         }
 
-        [HttpPost("add-cart/{id}")]
+        [HttpPost("add-cart")]
         public async Task<ActionResult<CartVO>> AddCart(CartVO vo)
         {
             var cart = await _repository.SaveOrUpdateCart(vo);
@@ -32,7 +32,7 @@ namespace Andreitoledo.GeekShopping.CartAPI.Controllers
             return Ok(cart);
         }
 
-        [HttpPut("update-cart/{id}")]
+        [HttpPut("update-cart")]
         public async Task<ActionResult<CartVO>> UpdateCart(CartVO vo)
         {
             var cart = await _repository.SaveOrUpdateCart(vo);
