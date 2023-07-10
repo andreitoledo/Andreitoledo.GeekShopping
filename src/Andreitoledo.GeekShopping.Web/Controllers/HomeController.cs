adfe4b61f3556e427fe1daff99aa02acf1e00032
andreitoledo <andreiltoledo@hotmail.com>
2023-07-10 20:23:03 -0300

    Implementado a Adição de Itens ao Carrinho

--- src/Andreitoledo.GeekShopping.Web/Controllers/HomeController.cs
+++ src/Andreitoledo.GeekShopping.Web/Controllers/HomeController.cs
@@ -9,18 +9,22 @@ namespace Andreitoledo.GeekShopping.Web.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
-
         private readonly IProductService _productService;
+        private readonly ICartService _cartService;
 
-        public HomeController(ILogger<HomeController> logger, IProductService productService)
+        public HomeController(ILogger<HomeController> logger,
+            IProductService productService,
+            ICartService cartService)
         {
             _logger = logger;
             _productService = productService;
+            _cartService = cartService;
         }
 
         public async Task<IActionResult> Index()
-        {            
+        {
             var products = await _productService.FindAllProducts("");
             return View(products);
         }
@@ -30,6 +34,40 @@ namespace Andreitoledo.GeekShopping.Web.Controllers
         {
             var token = await HttpContext.GetTokenAsync("access_token");
             var model = await _productService.FindProductById(id, token);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Details")]
+        [Authorize]
+        public async Task<IActionResult> DetailsPost(ProductViewModel model)
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+
+            CartViewModel cart = new()
+            {
+                CartHeader = new CartHeaderViewModel
+                {
+                    UserId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value
+                }
+            };
+
+            CartDetailViewModel cartDetail = new CartDetailViewModel()
+            {
+                Count = model.Count,
+                ProductId = model.Id,
+                Product = await _productService.FindProductById(model.Id, token)
+            };
+
+            List<CartDetailViewModel> cartDetails = new List<CartDetailViewModel>();
+            cartDetails.Add(cartDetail);
+            cart.CartDetails = cartDetails;
+
+            var response = await _cartService.AddItemToCart(cart, token);
+            if (response != null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
             return View(model);
         }
 
@@ -50,7 +88,6 @@ namespace Andreitoledo.GeekShopping.Web.Controllers
             var accessToken = await HttpContext.GetTokenAsync("access_token");
             return RedirectToAction(nameof(Index));
         }
-
         public IActionResult Logout()
         {
             return SignOut("Cookies", "oidc");
