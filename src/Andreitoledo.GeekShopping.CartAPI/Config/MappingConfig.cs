a0e9b9953fc8a7eed6695f628488444296776e38
andreitoledo <andreiltoledo@hotmail.com>
2023-07-09 21:55:38 -0300

    Configurando a Class Startup e DbContext do GeekShopping.Cart.API

---
+++ src/Andreitoledo.GeekShopping.CartAPI/Config/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿
+using Andreitoledo.GeekShopping.CartAPI.Model;
+using AutoMapper;
+
+namespace Andreitoledo.GeekShopping.CartAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig =  new MapperConfiguration(config =>
+            {
+                //config.CreateMap<ProductVO, Product>();
+                //config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+
+        }
+    }
+}
---
+++ src/Andreitoledo.GeekShopping.CartAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Andreitoledo.GeekShopping.CartAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext() {}
+        public MySQLContext(DbContextOptions<MySQLContext> options) 
+            : base(options) { }
+                
+        public DbSet<Product> Products { get; set; }
+     
+    }
+}
