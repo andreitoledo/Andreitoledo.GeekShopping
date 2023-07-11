093548cd79d852679f2bc0a225b717b463d78cde
andreitoledo <andreiltoledo@hotmail.com>
2023-07-11 16:38:29 -0300

    Implementando o Coupon Controller

---
+++ src/Andreitoledo.GeekShopping.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,29 @@
+﻿using Andreitoledo.GeekShopping.CouponAPI.Data.ValueObjects;
+using Andreitoledo.GeekShopping.CouponAPI.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Andreitoledo.GeekShopping.CouponAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private ICouponRepository _repository;
+
+        public CouponController(ICouponRepository repository)
+        {
+            _repository = repository ?? throw new
+                ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{couponCode}")]
+        [Authorize]
+        public async Task<ActionResult<CouponVO>> GetCouponByCouponCode(string couponCode)
+        {
+            var coupon = await _repository.GetCouponByCouponCode(couponCode);
+            if (coupon == null) return NotFound();
+            return Ok(coupon);
+        }
+    }
+}
