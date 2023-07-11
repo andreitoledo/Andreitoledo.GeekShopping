6e187ba61f2ecc55ed98b013b37573dfb27f70b5
andreitoledo <andreiltoledo@hotmail.com>
2023-07-11 13:52:43 -0300

    Configurando o Startup e o DbContext do GeekShopping.CartAPI Microservice

---
+++ src/Andreitoledo.GeekShopping.CouponAPI/Config/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace Andreitoledo.GeekShopping.CouponAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig =  new MapperConfiguration(config =>
+            {
+               // config.CreateMap<ProductVO, Product>().ReverseMap();
+               
+
+            });
+            return mappingConfig;
+
+        }
+    }
+}
---
+++ src/Andreitoledo.GeekShopping.CouponAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Andreitoledo.GeekShopping.CouponAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {        
+        public MySQLContext(DbContextOptions<MySQLContext> options) 
+            : base(options) { }
+                
+        public DbSet<Product> Products { get; set; }        
+     
+    }
+}
