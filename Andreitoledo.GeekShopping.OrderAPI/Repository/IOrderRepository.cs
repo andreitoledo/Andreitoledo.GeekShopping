479a200cc11d94d72d76d5c9d33d813bc281d4a9
andreitoledo <andreiltoledo@hotmail.com>
2023-07-23 22:20:15 -0300

    Implementando as Classes de Repositorio do Geek.ShoppingAPI

---
+++ Andreitoledo.GeekShopping.OrderAPI/Repository/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Andreitoledo.GeekShopping.OrderAPI.Model;
+
+namespace Andreitoledo.GeekShopping.OrderAPI.Repository
+{
+    public interface IOrderRepository
+    {
+
+        Task<bool> AddOrder(OrderHeader header);
+        Task UpdateOrderPaymentStatus(long orderHeaderId, bool paid);
+    }
+}
