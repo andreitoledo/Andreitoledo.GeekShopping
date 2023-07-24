ac35dbdd6476ba5708a40dbfb532918084a57365
andreitoledo <andreiltoledo@hotmail.com>
2023-07-23 22:45:44 -0300

    Iniciando a Implementação do RabbitMqCheckoutConsumer

---
+++ Andreitoledo.GeekShopping.OrderAPI/MessageConsumer/RabbitMQMessageConsumer.cs
@@ -0,0 +1,31 @@
+﻿using Andreitoledo.GeekShopping.OrderAPI.Repository;
+using RabbitMQ.Client;
+
+namespace Andreitoledo.GeekShopping.OrderAPI.MessageConsumer
+{
+    public class RabbitMQMessageConsumer : BackgroundService
+    {
+        private readonly OrderRepository _repository;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMQMessageConsumer(OrderRepository repository)
+        {
+            _repository = repository;
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "checkoutqueue", false, false, false, arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
