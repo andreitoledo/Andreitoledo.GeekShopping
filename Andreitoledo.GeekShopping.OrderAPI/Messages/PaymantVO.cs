193f516813cb7ae49157e78cbf5168f61f51769d
andreitoledo <andreiltoledo@hotmail.com>
2023-08-19 09:14:12 -0300

    Criação dos VO´s Representando a Mensagem Enviada ao RabbitMQ

---
+++ Andreitoledo.GeekShopping.OrderAPI/Messages/PaymantVO.cs
@@ -0,0 +1,15 @@
+﻿using Andreitoledo.GeekShopping.MessageBus;
+
+namespace Andreitoledo.GeekShopping.OrderAPI.Messages
+{
+    public class PaymantVO : BaseMessage
+    {
+        public long OrderId { get; set; }
+        public string Name { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string ExpiryMonthYear { get; set; }
+        public decimal PurchaseAmount { get; set; }
+        public string Email { get; set; }
+    }
+}
